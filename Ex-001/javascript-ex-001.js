0390085166270d3202a68511732f51c8a555e916
André Luis <andrelus475@gmail.com>
2024-11-02 14:52:23 -0300

    Create javascript-ex-001.js  in Ex-001

---
+++ Ex-001/javascript-ex-001.js
@@ -0,0 +1,9 @@
+function botao1(){
+    window.alert("Voce clicou no 1° Botão");
+}
+function botao2(){
+    window.alert("Voce clicou no 2° Botão");
+}
+function botao3(){
+    window.alert("Voce clicou no 3° Botão");
+}
