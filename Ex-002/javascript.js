733f008cc4eb3820b689f92e41a800a69d8c5004
André Luis <andrelus475@gmail.com>
2024-11-03 11:23:15 -0300

    Create javascript.js ex-002

---
+++ Ex-002/javascript.js
@@ -0,0 +1,8 @@
+function desafioiniciado(){
+    let nome;
+    let idade;
+
+    nome = window.prompt("Qual é o seu nome?");
+    idade = window.prompt("Olá " + nome + ", qual a sua idade?");
+    window.alert("Acabei de conhecer " + nome + " que tem " +  idade + " anos de idade!");
+}
